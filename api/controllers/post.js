fd69f70ec002a1529b18ca21561126ddcac0b3ff
James Geib <james@zuse.com>
2024-04-05 15:08:52 -0400

    successfully renderd post from database to frontend

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,3 +1,26 @@
+import { db } from "../db.js";
+
+export const getPosts = (req, res) => {
+    const q = req.query.cat ? "SELECT * FROM posts WHERE cat = ?" : "SELECT * FROM posts";
+
+    db.query(q, [req.query.cat], (err, result) => {
+        if (err) return res.send(err);
+        return res.status(200).json(result);
+    })
+};
+
+export const getPost = (req, res) => {
+    res.json("From post controller");
+};
+
 export const addPost = (req, res) => {
+    res.json("From post controller");
+};
+
+export const deletePost = (req, res) => {
+    res.json("From post controller");
+};
+
+export const updatePost = (req, res) => {
     res.json("From post controller");
 };
